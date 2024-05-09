63f070e37df5c4b94b43e5476b9a337acb7242cb
kanokwannar <168174770+kanokwannar@users.noreply.github.com>
2024-05-09 12:06:18 +0700

    change
    change

--- index.js
+++ index.js
@@ -3,29 +3,35 @@ let question1Answered = false;
 let question2Answered = false;
 
 function checkAnswer(questionNum, selectedAnswer) {
-    if (questionNum === 1) {
-        evaluateAnswer(questionNum, selectedAnswer);
-    } else if (questionNum === 2) {
-        evaluateAnswer(questionNum, selectedAnswer);
+    if (questionNum === 1 && !question1Answered) {
+        evaluateAnswer(questionNum, selectedAnswer, 'pim');
+        question1Answered = true; // ทำเครื่องหมายว่าคำถาม 1 ได้รับการตอบแล้ว
+    } else if (questionNum === 2 && !question2Answered) {
+        evaluateAnswer(questionNum, selectedAnswer, 'AI');
+        question2Answered = true; // ทำเครื่องหมายว่าคำถาม 2 ได้รับการตอบแล้ว
     } 
 }
 
-function evaluateAnswer(questionNum, selectedAnswer) {
-    let correctAnswer;
+function evaluateAnswer(questionNum, selectedAnswer, correctAnswer) {
+    let questionAnswered;
+
     if (questionNum === 1) {
-        correctAnswer = 'pim';
+        questionAnswered = question1Answered; // ตรวจสอบว่ามีการตอบคำถาม 1 แล้วหรือไม่
     } else if (questionNum === 2) {
-        correctAnswer = 'AI';
+        questionAnswered = question2Answered; // ตรวจสอบว่ามีการตอบคำถาม 2 แล้วหรือไม่
     }
 
-    if (selectedAnswer === correctAnswer) {
-        totalScore += 1;
-        updateScore();
-    } else {
-        if (totalScore > 0){
-            totalScore -= 1;
-            updateScore();
+    if (!questionAnswered) {
+        const buttonClass = selectedAnswer === correctAnswer ? 'answer' : 'wrong';
+        document.querySelector('.' + buttonClass + questionNum).classList.add('selected');
+        
+        if (selectedAnswer === correctAnswer) {
+            totalScore += 1;
+        } else {
+            totalScore = Math.max(0, totalScore - 1);
         }
+        
+        updateScore();
     }
 }
 
@@ -33,4 +39,11 @@ function evaluateAnswer(questionNum, selectedAnswer) {
 function updateScore() {
     const scoreElement = document.getElementById('total-score');
     scoreElement.textContent = totalScore;
+}
+
+function refreshPage() {
+    totalScore = 0;
+    question1Answered = false; // รีเซ็ตสถานะการตอบคำถาม 1
+    question2Answered = false; // รีเซ็ตสถานะการตอบคำถาม 2
+    updateScore();
 }
